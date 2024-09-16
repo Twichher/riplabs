896ddd525c547af6d49a1afc94fa7236f5b0b3a4
Twichher <vladickflorin@gmail.com>
2024-09-16 19:45:27 +0300

    seventh

--- main/views.py
+++ main/views.py
@@ -3,21 +3,43 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+def get_buck():
+    context_d = {
+        'title':'bucket',
+        'orders' : [
+            {'name':'Двигатель бензиновый DLE30', 'price': '42000 ₽', 'img':"http://127.0.0.1:9000/mysite2/1.jpg",'id': 1},
+            {'name':'Сервопривод EMAX ES08MDII', 'price': '1620 ₽', 'img':"http://127.0.0.1:9000/mysite2/2.jpg", 'id': 2},
+            {'name':'Двигатель T-Motor AT4120', 'price': '11980 ₽', 'img': "http://127.0.0.1:9000/mysite2/3.jpg", 'id': 3},
+        ]
+
+    }
+
+    return context_d
+
+def get_info(title):
+
+    context_d ={
+        'title': title,
+        'orders' : [
+            {'name':'Двигатель бензиновый DLE30', 'price': '42000 ₽', 'desc':'Бензиновый двигатель от известного производителя моторов DLE, рекомендованный для использования на небольших самолётах.', 'img': "http://127.0.0.1:9000/mysite2/1.jpg" ,'id': 1},
+            {'name':'Сервопривод EMAX ES08MDII', 'price': '1620 ₽', 'desc':'Небольшой по размерам и по мощности сервопривод, Futaba/JR-совместимый.', 'img': "http://127.0.0.1:9000/mysite2/2.jpg" , 'id': 2},
+            {'name':'Двигатель T-Motor AT4120', 'price': '11980 ₽', 'desc':'Бесколлекторный высокоэффективный двигатель, рекомендуемый к использованию с пропеллерами 15"-16".', 'img': "http://127.0.0.1:9000/mysite2/3.jpg", 'id': 3},
+            {'name':'Адаптер для пропеллера', 'price': '42770 ₽', 'desc':'Алюминиевый адаптер для тянущих пропеллеров. Устанавливается на вал диаметром 5мм. Используется для лопастей с осевым отверстием 3 мм.', 'img': "http://127.0.0.1:9000/mysite2/4.jpg", 'id': 4},
+            {'name':'Двигатель бензиновый DLE60', 'price': '152380 ₽', 'desc':'Бензиновый двигатель от известного производителя моторов DLE, рекомендованный для использования на больших самолётах, таких как VolJet VT10 и VolJet VT20.', 'img': "http://127.0.0.1:9000/mysite2/5.jpg", 'id': 5},
+            {'name':'Модуль для программ регуляторов', 'price': '4540 ₽', 'desc':'Модуль Hobbywing Program Card предназначен для программирования бесколлекторных регуляторов HobbyWing. Дружественный интерфейс делает программирование ESC простым и быстрым.', 'img': "http://127.0.0.1:9000/mysite2/6.jpg", 'id': 6},
+        ],
+    }
+        
+    return context_d
+
 
 
 def main(request):
-    context_d = {
-            'title': 'main',
-            'orders' : [
-                {'name':'Двигатель бензиновый DLE30', 'price': '42000 ₽', 'img':'<img class="img_goods" src="http://127.0.0.1:9000/mysite2/1.jpg">'  ,'id': 1},
-                {'name':'Сервопривод EMAX ES08MDII', 'price': '1620 ₽', 'img': '<img class="img_goods" src="http://127.0.0.1:9000/mysite2/2.jpg">', 'id': 2},
-                {'name':'Двигатель T-Motor AT4120', 'price': '11980 ₽', 'img': '<img class="img_goods" src="http://127.0.0.1:9000/mysite2/3.jpg">', 'id': 3},
-                {'name':'Адаптер для пропеллера', 'price': '42770 ₽', 'img': '<img class="img_goods" src="http://127.0.0.1:9000/mysite2/4.jpg">', 'id': 4},
-                {'name':'Двигатель бензиновый DLE60', 'price': '152380 ₽', 'img': '<img class="img_goods" src="http://127.0.0.1:9000/mysite2/5.jpg">', 'id': 5},
-                {'name':'Модуль для программ регуляторов', 'price': '4540 ₽', 'img': '<img class="img_goods" src="http://127.0.0.1:9000/mysite2/6.jpg">', 'id': 6},
-            ],
-            'value':'',
-    }
+
+    context_d = get_info('main')
+    context_d['buck'] = len(get_buck()['orders'])
+
+    print(context_d['buck'])
 
     if request.method == 'GET':
 
@@ -40,44 +62,25 @@ def main(request):
             if context_d['value'] in var['name']:
                 goods['orders'].append(var)
 
-        # for i in goods['orders']:
-        #     print(i['name'])
 
 
         return render(request, 'main.html', goods)
 
 
 def info(request, id):
-    context_d ={
-        'title':'info',
-        'desc' : [
-            {'name':'Двигатель бензиновый DLE30', 'price': '42000 ₽', 'desc':'Бензиновый двигатель от известного производителя моторов DLE, рекомендованный для использования на небольших самолётах.', 'img':'<img class="picG" src="http://127.0.0.1:9000/mysite2/1.jpg">'  ,'id': 1},
-            {'name':'Сервопривод EMAX ES08MDII', 'price': '1620 ₽', 'desc':'Небольшой по размерам и по мощности сервопривод, Futaba/JR-совместимый.', 'img': '<img class="picG" src="http://127.0.0.1:9000/mysite2/2.jpg">', 'id': 2},
-            {'name':'Двигатель T-Motor AT4120', 'price': '11980 ₽', 'desc':'Бесколлекторный высокоэффективный двигатель, рекомендуемый к использованию с пропеллерами 15"-16".', 'img': '<img class="picG" src="http://127.0.0.1:9000/mysite2/3.jpg">', 'id': 3},
-            {'name':'Адаптер для пропеллера', 'price': '42770 ₽', 'desc':'Алюминиевый адаптер для тянущих пропеллеров. Устанавливается на вал диаметром 5мм. Используется для лопастей с осевым отверстием 3 мм.', 'img': '<img class="picG" src="http://127.0.0.1:9000/mysite2/4.jpg">', 'id': 4},
-            {'name':'Двигатель бензиновый DLE60', 'price': '152380 ₽', 'desc':'Бензиновый двигатель от известного производителя моторов DLE, рекомендованный для использования на больших самолётах, таких как VolJet VT10 и VolJet VT20.', 'img': '<img class="picG" src="http://127.0.0.1:9000/mysite2/5.jpg">', 'id': 5},
-            {'name':'Модуль для программ регуляторов', 'price': '4540 ₽', 'desc':'Модуль Hobbywing Program Card предназначен для программирования бесколлекторных регуляторов HobbyWing. Дружественный интерфейс делает программирование ESC простым и быстрым.', 'img': '<img class="picG" src="http://127.0.0.1:9000/mysite2/6.jpg">', 'id': 6},
-        ]
-    }
+
+    context_d = get_info('info')
 
     con_ready = {
         'title':'info',
-        'desc': context_d['desc'][id-1]
+        'orders': context_d['orders'][id-1]
     }
 
     return render(request, 'info.html', con_ready)
 
 def bucket(request):
 
-    context_d = {
-        'title':'bucket',
-        'orders' : [
-            {'name':'Двигатель бензиновый DLE30', 'price': '42000 ₽', 'img':'<img class="imgor" src="http://127.0.0.1:9000/mysite2/1.jpg">'  ,'id': 1},
-            {'name':'Сервопривод EMAX ES08MDII', 'price': '1620 ₽', 'img': '<img class="imgor" src="http://127.0.0.1:9000/mysite2/2.jpg">', 'id': 2},
-            {'name':'Двигатель T-Motor AT4120', 'price': '11980 ₽', 'img': '<img class="imgor" src="http://127.0.0.1:9000/mysite2/3.jpg">', 'id': 3},
-        ]
-
-    }
+    context_d = get_buck()
 
     return render(request, 'bucket.html', context_d)
 
